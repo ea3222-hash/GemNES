8167e04b43d77481bbde0128ac8ed8262f83b62e
ea3222-hash <anaracom03@gmail.com>
2026-05-13 18:04:06 +0500

    terrible

--- PPU.cpp
+++ PPU.cpp
@@ -62,8 +62,6 @@ uint8_t PPU::cpuRead(uint16_t addr, uint8_t open_bus) {
     switch (addr & 0x0007) {
         case 0x0002: {
             data = (status & 0xE0) | (ppu_data_latch & 0x1F);
-            
-            // --- FIX: VBlank Start Timing ---
             if (scanline == 241) {
                 if (cycle == 0) { 
                     vblank_suppress = true; 
@@ -76,13 +74,9 @@ uint8_t PPU::cpuRead(uint16_t addr, uint8_t open_bus) {
                     nmi_suppressed = true; 
                 }
             }
-            
-            // --- FIX: VBlank End Timing ---
-            // If the CPU reads exactly on the dot the flag clears, it must see 0!
             if (scanline == -1 && cycle == 1) {
                 data &= ~0x80; 
             }
-            
             status &= ~0x80; 
             w = 0;           
             update_nmi();
@@ -104,15 +98,12 @@ uint8_t PPU::cpuRead(uint16_t addr, uint8_t open_bus) {
         case 0x0007:
             data = ppu_data_buffer;
             ppu_data_buffer = ppuRead(v);
-            
             if (v >= 0x3F00) {
                 data = (ppu_data_buffer & 0x3F) | (ppu_data_latch & 0xC0);
                 ppu_data_buffer = ppuRead(v & 0x2FFF); 
             }
-            
             bool is_rendering = (mask & 0x18) && (scanline >= 0 && scanline < 240);
             bool is_active_cycle = (cycle >= 1 && cycle <= 256) || (cycle >= 321 && cycle <= 336);
-            
             if (is_rendering && is_active_cycle) {
                 if ((v & 0x001F) == 31) { v &= ~0x001F; v ^= 0x0400; } else { v++; }
                 if ((v & 0x7000) != 0x7000) { v += 0x1000; }
@@ -127,7 +118,6 @@ uint8_t PPU::cpuRead(uint16_t addr, uint8_t open_bus) {
             }
             break;
     }
-    
     ppu_data_latch = data;
     latch_decay_timer = 3000000; 
     return data;
@@ -140,9 +130,7 @@ void PPU::cpuWrite(uint16_t addr, uint8_t data) {
         case 0x0000: control = data; t = (t & 0xF3FF) | ((data & 0x03) << 10); update_nmi(); break;
         case 0x0001: mask = data; break;
         case 0x0003: oam_addr = data; break;
-        case 0x0004: 
-            OAM[oam_addr++] = data; 
-            break;
+        case 0x0004: OAM[oam_addr++] = data; break;
         case 0x0005:
             if (w == 0) { x = data & 0x07; t = (t & 0xFFE0) | (data >> 3); w = 1; }
             else        { t = (t & 0x8FFF) | ((data & 0x07) << 12); t = (t & 0xFC1F) | ((data & 0xF8) << 2); w = 0; }
@@ -153,10 +141,8 @@ void PPU::cpuWrite(uint16_t addr, uint8_t data) {
             break;
         case 0x0007:
             ppuWrite(v, data);
-            
             bool is_rendering = (mask & 0x18) && (scanline >= 0 && scanline < 240);
             bool is_active_cycle = (cycle >= 1 && cycle <= 256) || (cycle >= 321 && cycle <= 336);
-            
             if (is_rendering && is_active_cycle) {
                 if ((v & 0x001F) == 31) { v &= ~0x001F; v ^= 0x0400; } else { v++; }
                 if ((v & 0x7000) != 0x7000) { v += 0x1000; }
@@ -168,7 +154,6 @@ void PPU::cpuWrite(uint16_t addr, uint8_t data) {
                 }
             } else { 
                 v += (control & 0x04) ? 32 : 1; 
-                
             }
             latch_decay_timer = 3000000;
             break;
@@ -192,11 +177,8 @@ uint8_t PPU::ppuRead(uint16_t addr) {
             else if (addr < 0x0800) data = nameTable[0][addr & 0x03FF];
             else if (addr < 0x0C00) data = nameTable[1][addr & 0x03FF];
             else                    data = nameTable[1][addr & 0x03FF];
-        } else if (cart->mirror == ONESCREEN_LO) {
-            data = nameTable[0][addr & 0x03FF]; 
-        } else if (cart->mirror == ONESCREEN_HI) {
-            data = nameTable[1][addr & 0x03FF]; 
-        }
+        } else if (cart->mirror == ONESCREEN_LO) { data = nameTable[0][addr & 0x03FF]; } 
+        else if (cart->mirror == ONESCREEN_HI) { data = nameTable[1][addr & 0x03FF]; }
     } 
     else if (addr >= 0x3F00 && addr <= 0x3FFF) {
         addr &= 0x001F;
@@ -205,11 +187,7 @@ uint8_t PPU::ppuRead(uint16_t addr) {
         else if (addr == 0x18) addr = 0x08;
         else if (addr == 0x1C) addr = 0x0C;
         data = paletteTable[addr] & 0x3F;
-
-        // FAIL 6 FIX: Greyscale physically masks out the chroma bits!
-        if (mask & 0x01) {
-            data &= 0x30; 
-        }
+        if (mask & 0x01) data &= 0x30; 
     }
     return data;
 }
@@ -229,11 +207,8 @@ void PPU::ppuWrite(uint16_t addr, uint8_t data) {
             else if (addr < 0x0800) nameTable[0][addr & 0x03FF] = data;
             else if (addr < 0x0C00) nameTable[1][addr & 0x03FF] = data;
             else                    nameTable[1][addr & 0x03FF] = data;
-        } else if (cart->mirror == ONESCREEN_LO) {
-            nameTable[0][addr & 0x03FF] = data; 
-        } else if (cart->mirror == ONESCREEN_HI) {
-            nameTable[1][addr & 0x03FF] = data; 
-        }
+        } else if (cart->mirror == ONESCREEN_LO) { nameTable[0][addr & 0x03FF] = data; } 
+        else if (cart->mirror == ONESCREEN_HI) { nameTable[1][addr & 0x03FF] = data; }
     } 
     else if (addr >= 0x3F00 && addr <= 0x3FFF) {
         addr &= 0x001F;
@@ -241,7 +216,6 @@ void PPU::ppuWrite(uint16_t addr, uint8_t data) {
         else if (addr == 0x14) addr = 0x04;
         else if (addr == 0x18) addr = 0x08;
         else if (addr == 0x1C) addr = 0x0C;
-        
         paletteTable[addr] = data & 0x3F; 
     }
 }
@@ -255,25 +229,30 @@ void PPU::step() {
 
         if ((cycle >= 1 && cycle <= 256) || (cycle >= 321 && cycle <= 336)) {
             if (rendering_enabled) {
-                bg_shifter_pattern_lo <<= 1; bg_shifter_pattern_hi <<= 1;
-                bg_shifter_attrib_lo <<= 1;  bg_shifter_attrib_hi <<= 1;
-            }
-            switch ((cycle - 1) % 8) {
-                case 0: 
-                    bg_shifter_pattern_lo = (bg_shifter_pattern_lo & 0xFF00) | bg_next_tile_lsb;
-                    bg_shifter_pattern_hi = (bg_shifter_pattern_hi & 0xFF00) | bg_next_tile_msb;
-                    bg_shifter_attrib_lo  = (bg_shifter_attrib_lo & 0xFF00) | ((bg_next_tile_attrib & 0b01) ? 0xFF : 0x00);
-                    bg_shifter_attrib_hi  = (bg_shifter_attrib_hi & 0xFF00) | ((bg_next_tile_attrib & 0b10) ? 0xFF : 0x00);
-                    break;
-                case 1: bg_next_tile_id = ppuRead(0x2000 | (v & 0x0FFF)); break;
-                case 3: 
-                    bg_next_tile_attrib = ppuRead(0x23C0 | (v & 0x0C00) | ((v >> 4) & 0x38) | ((v >> 2) & 0x07));
-                    if (v & 0x0040) bg_next_tile_attrib >>= 4;
-                    if (v & 0x0002) bg_next_tile_attrib >>= 2;
-                    bg_next_tile_attrib &= 0x03;
-                    break;
-                case 5: bg_next_tile_lsb = ppuRead(((control & 0x10) << 8) + ((uint16_t)bg_next_tile_id << 4) + ((v >> 12) & 0x07)); break;
-                case 7: bg_next_tile_msb = ppuRead(((control & 0x10) << 8) + ((uint16_t)bg_next_tile_id << 4) + ((v >> 12) & 0x07) + 8); break;
+                // FIX: BG Serial in. Shift registers bring in a 1.
+                bg_shifter_pattern_lo = (bg_shifter_pattern_lo << 1) | 1;
+                bg_shifter_pattern_hi = (bg_shifter_pattern_hi << 1) | 1;
+                bg_shifter_attrib_lo  = (bg_shifter_attrib_lo << 1) | 1;  
+                bg_shifter_attrib_hi  = (bg_shifter_attrib_hi << 1) | 1;
+            
+                // FIX: Stale BG Shift Registers. Fetch and reload only if rendering is active.
+                switch ((cycle - 1) % 8) {
+                    case 0: 
+                        bg_shifter_pattern_lo = (bg_shifter_pattern_lo & 0xFF00) | bg_next_tile_lsb;
+                        bg_shifter_pattern_hi = (bg_shifter_pattern_hi & 0xFF00) | bg_next_tile_msb;
+                        bg_shifter_attrib_lo  = (bg_shifter_attrib_lo & 0xFF00) | ((bg_next_tile_attrib & 0b01) ? 0xFF : 0x00);
+                        bg_shifter_attrib_hi  = (bg_shifter_attrib_hi & 0xFF00) | ((bg_next_tile_attrib & 0b10) ? 0xFF : 0x00);
+                        break;
+                    case 1: bg_next_tile_id = ppuRead(0x2000 | (v & 0x0FFF)); break;
+                    case 3: 
+                        bg_next_tile_attrib = ppuRead(0x23C0 | (v & 0x0C00) | ((v >> 4) & 0x38) | ((v >> 2) & 0x07));
+                        if (v & 0x0040) bg_next_tile_attrib >>= 4;
+                        if (v & 0x0002) bg_next_tile_attrib >>= 2;
+                        bg_next_tile_attrib &= 0x03;
+                        break;
+                    case 5: bg_next_tile_lsb = ppuRead(((control & 0x10) << 8) + ((uint16_t)bg_next_tile_id << 4) + ((v >> 12) & 0x07)); break;
+                    case 7: bg_next_tile_msb = ppuRead(((control & 0x10) << 8) + ((uint16_t)bg_next_tile_id << 4) + ((v >> 12) & 0x07) + 8); break;
+                }
             }
         }
 
@@ -295,10 +274,9 @@ void PPU::step() {
             if (scanline == -1 && cycle >= 280 && cycle < 305) v = (v & ~0x7BE0) | (t & 0x7BE0); 
         }
 
-        // --- FIX FOR FAIL 2: OAM Corruption happens at cycle 65! ---
         if (cycle == 65 && scanline >= -1 && scanline < 240) {
             if (rendering_enabled) {
-                eval_oam_addr = oam_addr; // Latch the address for evaluation
+                eval_oam_addr = oam_addr; 
                 if (eval_oam_addr >= 8) {
                     uint8_t row = eval_oam_addr & 0xF8;
                     for (int i = 0; i < 8; i++) OAM[row + i] = OAM[i];
@@ -308,102 +286,98 @@ void PPU::step() {
             }
         }
 
-        // --- FIX FOR FAIL 1 & 1: Misaligned OAM & Arbitrary Sprite 0 ---
+        // FIX: Sprites on scanline 0. OAM evaluation strictly requires rendering enabled.
         if (cycle == 257 && scanline >= -1 && scanline < 240) {
             if (rendering_enabled) {
-                oam_addr = 0; // Hardware unconditionally resets OAMADDR to 0
-            }
+                oam_addr = 0; 
+                memset(spriteScanline, 0xFF, sizeof(spriteScanline));
+                sprite_count = 0;
+                int spriteSize = (control & 0x20) ? 16 : 8; 
+                int eval_y = (scanline == -1) ? 255 : scanline;
 
-            memset(spriteScanline, 0xFF, sizeof(spriteScanline));
-            sprite_count = 0;
-            int spriteSize = (control & 0x20) ? 16 : 8; 
-            int eval_y = (scanline == -1) ? 255 : scanline;
-
-            int loops = 0;
-            while (loops < 64 && sprite_count < 9) {
-                // If misaligned, ALL sprites evaluate with a shifted byte offset!
-                int addr = (eval_oam_addr + loops * 4) % 256;
-                uint8_t y    = OAM[addr];
-                uint8_t id   = OAM[(addr + 1) % 256];
-                uint8_t attr = OAM[(addr + 2) % 256];
-                uint8_t x    = OAM[(addr + 3) % 256];
-                
-                int diff = eval_y - y;
-                if (diff >= 0 && diff < spriteSize) {
-                    if (sprite_count < 8) {
-                        spriteScanline[sprite_count].y = y;
-                        spriteScanline[sprite_count].id = id;
-                        spriteScanline[sprite_count].attribute = attr;
-                        spriteScanline[sprite_count].x = x;
-                        
-                        // Sprite Zero ONLY triggers if the data physically originated
-                        // from the first 4 bytes of OAM memory!
-                        spriteScanline[sprite_count].isSpriteZero = (addr < 4); 
+                int loops = 0;
+                while (loops < 64 && sprite_count < 9) {
+                    int addr = (eval_oam_addr + loops * 4) % 256;
+                    uint8_t y    = OAM[addr];
+                    uint8_t id   = OAM[(addr + 1) % 256];
+                    uint8_t attr = OAM[(addr + 2) % 256];
+                    uint8_t x    = OAM[(addr + 3) % 256];
+                    
+                    int diff = eval_y - y;
+                    if (diff >= 0 && diff < spriteSize) {
+                        if (sprite_count < 8) {
+                            spriteScanline[sprite_count].y = y;
+                            spriteScanline[sprite_count].id = id;
+                            spriteScanline[sprite_count].attribute = attr;
+                            spriteScanline[sprite_count].x = x;
+                            spriteScanline[sprite_count].isSpriteZero = (addr < 4); 
+                        }
+                        sprite_count++;
                     }
-                    sprite_count++;
+                    loops++;
                 }
-                loops++;
+                if (sprite_count > 8 && scanline >= 0) status |= 0x20; 
             }
-            if (sprite_count > 8 && scanline >= 0 && rendering_enabled) status |= 0x20; 
         }
 
+        // FIX: Stale Sprite Shift Registers. Treat X as 0 if rendering was disabled, and skip pattern fetch!
         if (cycle == 340 && scanline >= -1 && scanline < 240) {
             int safe_sprite_count = std::min((int)sprite_count, 8);
             int eval_y = (scanline == -1) ? 255 : scanline;
 
             for (int i = 0; i < safe_sprite_count; i++) {
-                uint8_t sprite_pattern_bits_lo, sprite_pattern_bits_hi;
-                uint16_t sprite_pattern_addr_lo, sprite_pattern_addr_hi;
-                bool flip_v = spriteScanline[i].attribute & 0x80;
-                bool flip_h = spriteScanline[i].attribute & 0x40;
+                if (!rendering_enabled) {
+                    spriteScanline[i].x = 0; 
+                } else {
+                    uint8_t sprite_pattern_bits_lo, sprite_pattern_bits_hi;
+                    uint16_t sprite_pattern_addr_lo, sprite_pattern_addr_hi;
+                    bool flip_v = spriteScanline[i].attribute & 0x80;
+                    bool flip_h = spriteScanline[i].attribute & 0x40;
 
-                int diff = eval_y - spriteScanline[i].y;
-                if (diff < 0 || diff > 15) diff = 0; 
+                    int diff = eval_y - spriteScanline[i].y;
+                    if (diff < 0 || diff > 15) diff = 0; 
 
-                if (!(control & 0x20)) { 
-                    if (!flip_v) sprite_pattern_addr_lo = ((control & 0x08) << 9) | (spriteScanline[i].id << 4) | diff;
-                    else         sprite_pattern_addr_lo = ((control & 0x08) << 9) | (spriteScanline[i].id << 4) | (7 - diff);
-                } else { 
-                    if (!flip_v) {
-                        if (diff < 8)
-                            sprite_pattern_addr_lo = ((spriteScanline[i].id & 0x01) << 12) | ((spriteScanline[i].id & 0xFE) << 4) | diff;
-                        else
-                            sprite_pattern_addr_lo = ((spriteScanline[i].id & 0x01) << 12) | (((spriteScanline[i].id & 0xFE) + 1) << 4) | (diff & 0x07);
-                    } else {
-                        if (diff < 8)
-                            sprite_pattern_addr_lo = ((spriteScanline[i].id & 0x01) << 12) | (((spriteScanline[i].id & 0xFE) + 1) << 4) | (7 - diff);
-                        else
-                            sprite_pattern_addr_lo = ((spriteScanline[i].id & 0x01) << 12) | ((spriteScanline[i].id & 0xFE) << 4) | (7 - (diff & 0x07));
+                    if (!(control & 0x20)) { 
+                        if (!flip_v) sprite_pattern_addr_lo = ((control & 0x08) << 9) | (spriteScanline[i].id << 4) | diff;
+                        else         sprite_pattern_addr_lo = ((control & 0x08) << 9) | (spriteScanline[i].id << 4) | (7 - diff);
+                    } else { 
+                        if (!flip_v) {
+                            if (diff < 8)
+                                sprite_pattern_addr_lo = ((spriteScanline[i].id & 0x01) << 12) | ((spriteScanline[i].id & 0xFE) << 4) | diff;
+                            else
+                                sprite_pattern_addr_lo = ((spriteScanline[i].id & 0x01) << 12) | (((spriteScanline[i].id & 0xFE) + 1) << 4) | (diff & 0x07);
+                        } else {
+                            if (diff < 8)
+                                sprite_pattern_addr_lo = ((spriteScanline[i].id & 0x01) << 12) | (((spriteScanline[i].id & 0xFE) + 1) << 4) | (7 - diff);
+                            else
+                                sprite_pattern_addr_lo = ((spriteScanline[i].id & 0x01) << 12) | ((spriteScanline[i].id & 0xFE) << 4) | (7 - (diff & 0x07));
+                        }
                     }
-                }
 
-                sprite_pattern_addr_hi = sprite_pattern_addr_lo + 8;
-                sprite_pattern_bits_lo = ppuRead(sprite_pattern_addr_lo);
-                sprite_pattern_bits_hi = ppuRead(sprite_pattern_addr_hi);
+                    sprite_pattern_addr_hi = sprite_pattern_addr_lo + 8;
+                    sprite_pattern_bits_lo = ppuRead(sprite_pattern_addr_lo);
+                    sprite_pattern_bits_hi = ppuRead(sprite_pattern_addr_hi);
 
-                if (flip_h) {
-                    auto flipbyte = [](uint8_t b) {
-                        b = (b & 0xF0) >> 4 | (b & 0x0F) << 4;
-                        b = (b & 0xCC) >> 2 | (b & 0x33) << 2;
-                        b = (b & 0xAA) >> 1 | (b & 0x55) << 1;
-                        return b;
-                    };
-                    sprite_pattern_bits_lo = flipbyte(sprite_pattern_bits_lo);
-                    sprite_pattern_bits_hi = flipbyte(sprite_pattern_bits_hi);
+                    if (flip_h) {
+                        auto flipbyte = [](uint8_t b) {
+                            b = (b & 0xF0) >> 4 | (b & 0x0F) << 4;
+                            b = (b & 0xCC) >> 2 | (b & 0x33) << 2;
+                            b = (b & 0xAA) >> 1 | (b & 0x55) << 1;
+                            return b;
+                        };
+                        sprite_pattern_bits_lo = flipbyte(sprite_pattern_bits_lo);
+                        sprite_pattern_bits_hi = flipbyte(sprite_pattern_bits_hi);
+                    }
+                    sprite_shifter_pattern_lo[i] = sprite_pattern_bits_lo;
+                    sprite_shifter_pattern_hi[i] = sprite_pattern_bits_hi;
                 }
-                sprite_shifter_pattern_lo[i] = sprite_pattern_bits_lo;
-                sprite_shifter_pattern_hi[i] = sprite_pattern_bits_hi;
             }
         }
     }
 
     if (scanline == 241 && cycle == 1) {
         status |= 0x80; 
-        
-        if (vblank_suppress) {
-            status &= ~0x80; 
-        }
-        
+        if (vblank_suppress) status &= ~0x80; 
         update_nmi();
         vblank_suppress = false; 
     }
@@ -487,7 +461,7 @@ void PPU::step() {
     if (latch_decay_timer > 0) {
         latch_decay_timer--;
         if (latch_decay_timer == 0) {
-            ppu_data_latch = 0x00; // The bus capacitance decayed!
+            ppu_data_latch = 0x00;
         }
     }
 
